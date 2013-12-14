56fa468bd884a07eb4573a95f2ef817a5ea1b152
shadowsky <sbdjamel@hotmail.com>
2013-12-15 00:10:34 +0100

    modif

--- SmallWorld/src/Giraffe.java
+++ SmallWorld/src/Giraffe.java
@@ -1,2 +1,3 @@
 public class Giraffe extends EspeceTer implements Herbivore {
+
 }
--- SmallWorld/src/InterfaceMonde.java
+++ SmallWorld/src/InterfaceMonde.java
@@ -1,8 +1,6 @@
-import java.util.Vector;
-
 public class InterfaceMonde {
 
-  public Monde monde;
+	public Monde monde;
 
     public Vector  1;
     public Monde myMonde;
