df362ac15ec56d26e01a1df7d4642cb0757f3a05
shadowsky <sbdjamel@hotmail.com>
2013-12-15 06:38:37 +0100

    correction constructeur

--- SmallWorld/src/especes/EspeceTer.java
+++ SmallWorld/src/especes/EspeceTer.java
@@ -1,19 +1,4 @@
 package especes;
 public class EspeceTer extends Espece implements Terrestre {
 
-	public EspeceTer() {
-		super();
-	}
-
-	public EspeceTer(Espece espece) {
-		super(espece);
-	}
-
-	public EspeceTer(String nom, Boolean nocturne, Boolean sommeil, int vitesse, int force, int energie, Meute meute,
-			int vitesseCourse, Boolean sexe, Boolean estLeader, Boolean nage, Boolean estVivant, int champVision, int tempIdeale,
-			int nbReproductions, Boolean fuite) {
-		super(nom, nocturne, sommeil, vitesse, force, energie, meute, vitesseCourse,
-				sexe, estLeader, nage, estVivant, champVision, tempIdeale,
-				nbReproductions, fuite);
-	}
 }
