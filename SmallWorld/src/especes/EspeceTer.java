ea90e1134aac7aa9e17c2cc0de62ad8688f65d4d
shadowsky <sbdjamel@hotmail.com>
2013-12-15 18:29:05 +0100

    definition des constructeurs

--- SmallWorld/src/especes/EspeceTer.java
+++ SmallWorld/src/especes/EspeceTer.java
@@ -1,4 +1,12 @@
 package especes;
 public class EspeceTer extends Espece implements Terrestre {
 
+	EspeceTer(String nom, boolean sommeil, int vitesse, int force, int vitesseCourse, boolean estLeader, boolean nage, int champVison, int tempIdeal, int nbReproductions){
+		super(nom, sommeil, vitesse, force, vitesseCourse, estLeader, nage, champVison, tempIdeal, nbReproductions);
+	}
+
+	EspeceTer(Espece espece) {
+		super(espece);
+	}
+	
 }
